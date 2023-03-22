2c9b53829a7273c4d32629cca4d81f90993dbc6a
Parth Soneji <parthsoneji.softrefine@gmail.com>
2023-03-22 18:48:46 +0530

    First Commit

---
+++ lib/homePage.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:getx2/carController.dart';
+
+import 'controller.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final shoppingController = Get.put(ShoppingController());
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: Column(
+        children: [
+          Expanded(
+            child: GetX<ShoppingController>(
+              builder: (controller) {
+                return ListView.builder(
+                  itemCount: controller.products.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      margin: const EdgeInsets.all(12),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Row(
+                              mainAxisAlignment:
+                              MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment:
+                                  CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '${controller.products[index].productName}',
+                                      style: TextStyle(fontSize: 24),
+                                    ),
+                                    Text(
+                                        '${controller.products[index].productDescription}'),
+                                  ],
+                                ),
+                                Text('\$${controller.products[index].price}',
+                                    style: TextStyle(fontSize: 24)),
+                              ],
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                              //  cartController.addToCart(controller.products[index]);
+                              },
+                              child: Text('Add to Cart',style: TextStyle(color: Colors.white)),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+          Text('Total Amount: '),
+          SizedBox(height: 100),
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:getx2/homePage.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
